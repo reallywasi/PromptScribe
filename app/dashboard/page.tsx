a7aa520d13907b1b67e2f05371caca103cdf2dea
Mohammad Wasi <reallywasi@example.com>
2024-07-17 15:22:04 +0530

    Added Search Functionality to dashboard using proper props in templates and search

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client"
 
+import React,{useState} from 'react'
+// import { Search } from 'lucide-react'
+import SearchSection from './_components/SearchSection'
+import TemplateListSection from './_components/TemplateListSection'
 function Dashboard (){
+    const [userSearchInput,setUserSearchInput]=useState<string>()
   return (
     <div>
-      Dashboard
+<SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
+<TemplateListSection userSearchInput={userSearchInput}/>
     </div>
   )
 }
