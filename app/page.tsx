7467197dee052d5ea90eea95ff04d3190adb41b6
Mohammad Wasi <reallywasi@example.com>
2024-07-17 11:10:09 +0530

    Authentication Done using Clerk AUth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>hii</div>
+    <div>
+      <h1>Wasi</h1>
+      <Button>Wasi</Button>
+    </div>
   );
 }
